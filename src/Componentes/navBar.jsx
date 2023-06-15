16e546f492a1ccd46d513128354359860bc0121a
Maximiliano <mavzdev@gmail.com>
2023-06-15 10:52:37 -0300

    hice todo de nuevo jajajaj

--- src/Componentes/navBar.jsx
+++ src/Componentes/navBar.jsx
@@ -5,15 +5,21 @@ const Navbar = () =>{
     
 
     return (
-        <header className="headerNav">
-            <nav className="nav">
-                <ul>
-                    <a href=""><li>RYMA COMPUTACION</li></a>
-                    <a href=""><li>CATEGORIAS</li></a>
-                    <a href=""><li>CONTACTANOS</li></a>
-                </ul>
-            </nav>
-        </header>
+        <header className="header">
+        <nav className="navbar">
+            <ul className="navUl">
+                <li><a href="">HOME</a></li>
+                <li className="categorias"><a href="">CATEGORIS</a>
+                    <ul className="subCategorias">
+                        <li>computers</li>
+                        <li>mouses</li>
+                        <li>keyboards</li>
+                    </ul>
+                </li>
+                <li><a href="">CONTACT</a></li>
+            </ul>
+        </nav>
+    </header>
     )
 }
 export default Navbar;
